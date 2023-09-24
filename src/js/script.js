fc4344e287b27f635adcc5bce1a2a5da83d623b2
Wanderson duarte <wanderson.duarte_17@hotmail.com>
2023-09-23 21:03:29 -0300

    feat: "add the feature of generating advice"

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
+let adviceNumber = document.querySelector("#advice");
+let adviceDescription = document.querySelector("#advice__description");
+let button = document.querySelector("#button");
 
+function doSortAdiceNumber() {
+  let sortNumber = Math.floor(Math.random() * 200);
+  doAcessApi(sortNumber);
+}
+
+async function doAcessApi(sortNumber) {
+  let url = await fetch(`https://api.adviceslip.com/advice/${sortNumber}`);
+  let response = await url.json();
+  doRenderToAdvice(response);
+}
+function doRenderToAdvice(response) {
+  let adviceId = response.slip.id;
+  let advice = response.slip.advice;
+  adviceNumber.innerHTML = adviceId;
+  adviceDescription.innerHTML = advice;
+}
+
+button.addEventListener("click", doSortAdiceNumber);
